5592e88fd77aeafb62b6bb19ffd38fe9801cda04
Gideon <sitienei.gedion@gmail.com>
2023-03-21 18:46:14 +0300

    taking multiple inputs from the user

---
+++ ComputeAverage.py
@@ -0,0 +1,10 @@
+#prompt the user to enter 3 numbers
+Number1 = eval(input("Enter the first number: "))
+Number2 = eval(input("Enter the second number: "))
+Number3 = eval(input("Enter the third number: "))
+
+#compute the average
+Average = (Number1 + Number2 + Number3) / 3
+
+#Display the result
+print("The average of", Number1, Number2, Number3, "is", Average)
