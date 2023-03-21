93cec0c837b8d9accd58bd31238326b5e416cb4e
Gideon <sitienei.gedion@gmail.com>
2023-03-21 18:30:48 +0300

    taking input from the user

---
+++ ComputeAreaWithConsoleInput.py
@@ -0,0 +1,8 @@
+# prompt the user to enter radius
+radius = eval(input("Enter the radius: "))
+
+# compute area
+area = radius * radius * 3.14159
+
+# Display results
+print("The area for the circle of radius", radius, "is", area)
