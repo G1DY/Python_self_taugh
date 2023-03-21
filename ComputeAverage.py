1f6d96c54a9213aaf5a5cae327318e5ea8699e6f
Gideon <sitienei.gedion@gmail.com>
2023-03-21 19:25:32 +0300

    Prompting the user to enter numbers 1 by 1 and simultaneously

--- ComputeAverage.py
+++ ComputeAverage.py
@@ -1,7 +1,10 @@
 #prompt the user to enter 3 numbers
-Number1 = eval(input("Enter the first number: "))
+'''Number1 = eval(input("Enter the first number: "))
 Number2 = eval(input("Enter the second number: "))
-Number3 = eval(input("Enter the third number: "))
+Number3 = eval(input("Enter the third number: "))'''
+
+#prompt the user to enter three  numbers simultaneously
+Number1, Number2, Number3 = eval(input("Enter three numbers separated by a comma: "))
 
 #compute the average
 Average = (Number1 + Number2 + Number3) / 3
