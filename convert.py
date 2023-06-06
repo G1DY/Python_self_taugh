2432bab5f8efb05d3721cfe0597c646b7ac0cab8
Gideon <sitienei.gedion@gmail.com>
2023-06-06 12:05:58 +0300

    convert temp to fahrenheit

---
+++ convert.py
@@ -0,0 +1,5 @@
+def main():
+    celcius = eval(input("Please enter the temperature in degree celcius: "))
+    fahrenheit = (9 / 5) * celcius + 32
+    print("The converted temperature is", fahrenheit, "degrees fahrenheit")
+main()
