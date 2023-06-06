b52c44d056ce8fbd071f3c292538099e4972c5a0
Gideon <sitienei.gedion@gmail.com>
2023-06-06 13:00:51 +0300

    prints the average

---
+++ average.py
@@ -0,0 +1,6 @@
+def main():
+    print("This program prints the average of two scores")
+    score1, score2 = eval(input("The two scores separated by a comma: "))
+    average = (score1 + score2) / 2
+    print("Average is ", average)
+main()
