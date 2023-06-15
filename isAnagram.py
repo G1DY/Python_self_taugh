26e1049f0ce4ddb1bade0a9e21f6ba4aedc66b1e
Gideon <sitienei.gedion@gmail.com>
2023-06-15 15:04:04 +0300

    checks for an anagram

---
+++ isAnagram.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+        return counter_s == counter_t
+def main():
+    solution = Solution()
+    is_anagram = solution.isAnagram("diagram", "gramiad")
+    print(is_anagram)
+main()
